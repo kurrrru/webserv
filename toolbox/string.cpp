df40ac4cd4faf448ab74a19bc94957a6bde1d0dc
yu-432 <o.shimayu891@gmail.com>
2025-04-14 17:09:33 +0900

    chore: move caseInsensitiveCompare trim to toolbox ~

--- toolbox/string.cpp
+++ toolbox/string.cpp
@@ -9,4 +9,34 @@ namespace toolbox {
         ss << val;
         return ss.str();
     }
+
+    std::string trim(std::string& src, const std::string& sep) {
+        if (src.length() == 0) {
+            return "";
+        }
+        std::size_t pos = src.find(sep);
+        std::string newStr;
+        if (pos == std::string::npos) {
+            src.swap(newStr);
+            src.clear();
+        } else {
+            newStr = src.substr(0, pos);
+            src.erase(0, pos + sep.length());
+        }
+        return newStr;
+    }
+
+    bool caseInsensitiveCompare(const std::string& str1, const std::string& str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+        if (str1 == str2) {
+            return true;
+        }
+        return std::equal(str1.begin(), str1.end(), str2.begin(), str2.end(),
+                          [](char c1, char c2) {
+                              return std::tolower(static_cast<unsigned char>(c1)) ==
+                                     std::tolower(static_cast<unsigned char>(c2));
+                          });
+    }
 }
