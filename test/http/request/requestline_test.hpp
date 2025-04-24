20dea80bd924d5e30e6d721bcdf691c98399f1be
yu-432 <o.shimayu891@gmail.com>
2025-04-24 19:24:45 +0900

    fix: test error fix ~

--- test/http/request/requestline_test.hpp
+++ test/http/request/requestline_test.hpp
@@ -10,14 +10,32 @@
 #include "../../../src/http/request/request_parser.hpp"
 #include "../../../src/http/request/http_fields.hpp"
 #include "../../../src/http/http_namespace.hpp"
-// #include "../../../src/http/http_status.hpp"
+#include "../../../src/http/http_status.hpp"
+#include "base_test.hpp"
 
-typedef std::vector<RequestLineTest> TestVector;
 typedef std::pair<std::string, std::string> QueryPair;
 typedef std::vector<QueryPair> QueryVec;
 typedef std::map<std::string, std::string> QueryMap;  // 修正予定
 
-class RequestLineTest {
+// class RequestLineTest {
+//  public:
+//     struct ExceptRequestLine {
+//         std::string method;
+//         std::string path;
+//         QueryVec queryVec;
+//         std::string version;
+//     };
+
+//     RequestLineTest() : _httpStatus(http::OK) {}
+
+//     std::string _name;
+//     std::string _request;
+//     bool _isSuccessTest;
+//     http::HttpStatus _httpStatus;
+//     ExceptRequestLine _exceptRequest;
+// };
+
+class RequestLineTest : public BaseTest {
  public:
     struct ExceptRequestLine {
         std::string method;
@@ -25,12 +43,8 @@ class RequestLineTest {
         QueryVec queryVec;
         std::string version;
     };
+    RequestLineTest() : BaseTest() {}
+    ~RequestLineTest() {}
 
-    RequestLineTest() : _httpStatus(200) {}
-
-    std::string _name;
-    std::string _request;
-    bool _isSuccessTest;
-    std::size_t _httpStatus;
     ExceptRequestLine _exceptRequest;
 };
--- test/http/request/test_main.cpp
+++ test/http/request/test_main.cpp
@@ -2,6 +2,8 @@
 
 #include "../../../toolbox/stepmark.hpp"
 
+void requestLineTest();
+
 namespace http {
 void fieldTest();
 }
@@ -10,5 +12,7 @@ int main(void) {
     toolbox::logger::StepMark::setLogFile("field_test.log");
     toolbox::logger::StepMark::setLevel(toolbox::logger::DEBUG);
     http::fieldTest();
+    requestLineTest();
+
     return 0;
 }
