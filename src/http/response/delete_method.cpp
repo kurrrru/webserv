c5b84db94247c2c1a5e819ae5f6ebe314471f271
yu-432 <o.shimayu891@gmail.com>
2025-05-13 16:27:22 +0900

    feat: touch delete method ~

---
+++ src/http/response/delete_method.cpp
@@ -0,0 +1,10 @@
+#include "delete_method.hpp"
+
+namespace http {
+
+HttpStatus::EHttpStatus runDelete(const std::string& path, Response& response) {
+    HttpStatus::EHttpStatus status;
+    return status;
+}
+
+}  // namespace http
---
+++ src/http/response/delete_method.hpp
@@ -0,0 +1,8 @@
+#include "../http_status.hpp"
+#include "../http_namespace.hpp"
+#include "response.hpp"
+#include "method_utils.hpp"
+
+namespace http {
+void runDelete(std::string& path, Response& response);
+}
