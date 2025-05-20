7ad1cd5ae38a910bc37f134e5582f563c843e602
oonisidesu <eeeelllleeee24@gmail.com>
2025-05-20 12:48:13 +0900

    feat: add additional test cases for CGI handling in main.cpp

--- test/http/cgi/main.cpp
+++ test/http/cgi/main.cpp
@@ -1,12 +1,20 @@
 #include "../../../src/http/cgi/cgi_response_parser.hpp"
 #include "cgi_response_test.hpp"
+#include "cgi_handle_test.hpp"
+#include "cgi_execute_test.hpp"
 
 void requestLineTest();
+// void testCgiBufSize();
+void handleTest();
+void executeTest();
 
 int main(void) {
     toolbox::logger::StepMark::setLogFile("cgi_test.log");
     toolbox::logger::StepMark::setLevel(toolbox::logger::DEBUG);
     requestLineTest();
+    // testCgiBufSize();
+    handleTest();
+    executeTest();
 
     return 0;
 }
