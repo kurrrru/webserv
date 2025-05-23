0d73c0f497f5c63a24a7e351cdfc19f1e848720c
kurrrru <kurrrru8850@gmail.com>
2025-05-23 17:40:06 +0900

    create the prototype of request class

---
+++ src/http/request/request.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "request_parser.hpp"
+#include "../response/response.hpp"
+#include "../../config/config.hpp"
+#include "../../socket/client.hpp"
+#include "../../../toolbox/shared.hpp"
+
+namespace http {
+
+class Request {
+ public:
+    Request(toolbox::SharedPtr<Client> client);
+    ~Request();
+    void parseRawRequest(const std::string& rawRequest);
+    void fetchConfig();
+    void handleRequest();
+    void sendResponse() const;
+
+ private:
+    http::RequestParser _parsedRequest;
+    config::LocationConfig _config;
+    http::Response _response;
+    toolbox::SharedPtr<Client> _client;
+
+    Request();
+    Request(const Request& other);
+    Request& operator=(const Request& other);
+};
+
+}
