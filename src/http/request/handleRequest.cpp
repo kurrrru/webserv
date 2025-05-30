dd2cc06a865a94605671a63c4745ed5cf33c3564
yu-432 <o.shimayu891@gmail.com>
2025-05-30 11:47:12 +0900

    refactor: delete try catch ~

--- src/http/request/handleRequest.cpp
+++ src/http/request/handleRequest.cpp
@@ -8,41 +8,34 @@
 
 namespace http {
 void Request::handleRequest() {
-    try {
-        HTTPRequest& httpRequest = _parsedRequest.get();
-        const std::vector<std::string>& allowedMethods = _config.getAllowedMethods();
+    HTTPRequest& httpRequest = _parsedRequest.get();
+    const std::vector<std::string>& allowedMethods = _config.getAllowedMethods();
 
-        if (std::find(allowedMethods.begin(), allowedMethods.end(),
-                      httpRequest.method) == allowedMethods.end()) {
-            _response.setStatus(HttpStatus::METHOD_NOT_ALLOWED);
-            toolbox::logger::StepMark::error(
-                "Request::handleRequest: Method not allowed: "
-                    + httpRequest.method);
-            return;
-        }
-
-        CgiHandler cgiHandler;
-
-        const std::string& targetPath = httpRequest.uri.path;
-        const std::vector<std::string>& cgiExtensionVector
-            = _config.getCgiExtensions();
-        const std::string& cgiPath = _config.getCgiPath();
-
-        if (cgiHandler.isCgiRequest(targetPath, cgiExtensionVector, cgiPath)) {
-            cgiHandler.handleRequest(httpRequest, _response,
-                _config.getRoot(), cgiExtensionVector, cgiPath);
-        } else {
-            serverMethod::serverMethodHandler(
-                _parsedRequest, _config, httpRequest.fields, _response);
-        }
-
-        toolbox::logger::StepMark::info("Request::handleRequest: Request processed successfully");
-    } catch (const std::exception& e) {
+    if (std::find(allowedMethods.begin(), allowedMethods.end(),
+                    httpRequest.method) == allowedMethods.end()) {
+        _response.setStatus(HttpStatus::METHOD_NOT_ALLOWED);
         toolbox::logger::StepMark::error(
-            "Request::handleRequest: Exception occurred: "
-            + std::string(e.what()));
-        _response.setStatus(HttpStatus::INTERNAL_SERVER_ERROR);
+            "Request::handleRequest: Method not allowed: "
+                + httpRequest.method);
+        return;
     }
+
+    CgiHandler cgiHandler;
+
+    const std::string& targetPath = httpRequest.uri.path;
+    const std::string& cgiPath = _config.getCgiPath();
+    const std::vector<std::string>& cgiExtensionVector = _config.getCgiExtensions();
+
+    if (cgiHandler.isCgiRequest(targetPath, cgiExtensionVector, cgiPath)) {
+        cgiHandler.handleRequest(httpRequest, _response,
+            _config.getRoot(), cgiExtensionVector, cgiPath);
+    } else {
+        serverMethod::serverMethodHandler(
+            _parsedRequest, _config, httpRequest.fields, _response);
+    }
+
+    toolbox::logger::StepMark::info("Request::handleRequest: handled request for "
+        + httpRequest.uri.path + " with method " + httpRequest.method);
 }
 
 }  // namespace http
