a86a17b28af78f849f1fcf0d7975a45b7d79a14e
kurrrru <kurrrru8850@gmail.com>
2025-05-31 16:59:55 +0900

    add impl of sendResponse

--- src/http/request/request_impl_response.cpp
+++ src/http/request/request_impl_response.cpp
@@ -15,6 +15,54 @@ namespace {
         }
         return "-";
     }
+
+    void propagateErrorPage(
+        http::Response* response, const http::Response& errorResponse) {
+        typedef std::string FieldName;
+        typedef std::string FieldDefaultValue;
+
+        // [TODO] ここも後で確認します
+        const std::pair<std::string, std::string> defaultHeadersArray[] = {
+            {http::fields::CONTENT_TYPE, "text/plain"},
+            {http::fields::CACHE_CONTROL, "no-cache"},
+            {http::fields::CONNECTION, "close"},
+            {http::fields::SERVER, "WebServer/1.0"}
+        };
+        const std::vector<std::pair<FieldName, FieldDefaultValue> > defaultHeaders(
+            defaultHeadersArray, defaultHeadersArray + sizeof(defaultHeadersArray)
+            / sizeof(defaultHeadersArray[0]));
+
+        for (std::size_t i = 0; i < defaultHeaders.size(); ++i) {
+            const std::pair<FieldName, FieldDefaultValue>& header =
+                defaultHeaders[i];
+            std::string headerValue = errorResponse.getHeader(header.first);
+            if (headerValue.empty()) {
+                headerValue = header.second;
+            }
+            response->setHeader(header.first, headerValue);
+        }
+        response->setBody(errorResponse.getBody());
+    }
+
+    void setDefaultErrorPage(http::Response* response, std::size_t status) {
+        // [TODO] 設定すべきフィールドを確認する
+        // [TODO] デフォルトのエラーページを設定する
+        response->setHeader(http::fields::CONTENT_TYPE, "text/html");
+        response->setStatus(status);
+        std::stringstream ss;
+        ss << "<!DOCTYPE html>\n"
+            << "<html>\n"
+            << "<head>\n"
+            << "<title>Error " << status << "</title>\n"
+            << "</head>\n"
+            << "<body>\n"
+            << "<h1>Error " << status << "</h1>\n"
+            << "<p>An error occurred while processing your request.</p>\n"
+            << "</body>\n"
+            << "</html>";
+        response->setBody(ss.str());
+    }
+
 }
 
 void http::Request::sendResponse() {
@@ -23,10 +71,19 @@ void http::Request::sendResponse() {
     if (status >= config::directive::MIN_ERROR_PAGE_CODE
             && status <= config::directive::MAX_ERROR_PAGE_CODE) {
         std::vector<config::ErrorPage> errorPages = _config.getErrorPages();
+        bool useDefaultErrorPage = true;
         for (std::size_t i = 0; i < errorPages.size(); ++i) {
             if (std::find(errorPages[i].getCodes().begin(),
                     errorPages[i].getCodes().end(), status)
                 != errorPages[i].getCodes().end()) {
+                // [TODO] Change the processing of the error page
+                // depending on the prefix of the error page path
+                // "/" - ngx_http_internal_redirect
+                // "@" - ngx_http_named_location
+                // otherwise - ngx_http_send_refresh or ngx_http_send_special_response
+
+                // [TODO] この辺は後で書きます
+
                 http::Request errorRequest(_client, _requestDepth + 1);
                 std::string method = http::method::GET;
                 std::string path = errorPages[i].getPath();
@@ -42,20 +99,15 @@ void http::Request::sendResponse() {
                 errorRequest.fetchConfig();
                 errorRequest.handleRequest();
                 if (errorRequest._response.getStatus() == http::HttpStatus::OK) {
-                    // content-type も設定する必要あり
-                    _response.setBody(errorRequest._response.getBody());
-                } else {
-                    // _response.setHeader(http::fields::CONTENT_TYPE,
-                    //     "text/html");
-                    // _response.setBody("<html><body>"
-                    //     "<h1>Error: " + std::to_string(status) + "</h1>"
-                    //     "<p>Internal error occurred.</p></body></html>");
+                    propagateErrorPage(&_response, errorRequest._response);
+                    useDefaultErrorPage = false;
                 }
             }
         }
+        if (useDefaultErrorPage) {
+            setDefaultErrorPage(&_response, status);
+        }
     } 
-
-
 
     std::string remote_addr = _client->getIp();
     std::string remote_user = "-";
