9e95ca69269650ab2d3f87634655aba13eff0b8e
yu-432 <o.shimayu891@gmail.com>
2025-06-03 13:43:12 +0900

    fix: comment out unused variable`

--- toolbox/access.cpp
+++ toolbox/access.cpp
@@ -20,9 +20,9 @@ The class is not thread-safe, so it should not be used in a multi-threaded envir
 #include <stdexcept>
 #include <string>
 
-namespace {
-    const std::size_t IO_BUFFER_SIZE = 16 * 1024; // 16 KB
-}
+// namespace {
+//     const std::size_t IO_BUFFER_SIZE = 16 * 1024; // 16 KB
+// }
 
 namespace toolbox {
 
