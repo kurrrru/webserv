589a0313827dbac27b8cd416cfc18972709a48f3
kurrrru <kurrrru8850@gmail.com>
2025-06-08 13:24:10 +0900

    format

--- src/http/request/request.cpp
+++ src/http/request/request.cpp
@@ -5,29 +5,29 @@ namespace http {
 void Request::run() {
     const std::size_t root_depth = 0;
     switch (_ioPendingState) {
-    case NO_IO_PENDING:
-    case REQUEST_READING:
-        if (_requestDepth == root_depth) {
-            recvRequest();
+        case NO_IO_PENDING:
+        case REQUEST_READING:
+            if (_requestDepth == root_depth) {
+                recvRequest();
+                if (_ioPendingState != NO_IO_PENDING)
+                    break;
+            }
+        // fallthrough
+        case CGI_BODY_SENDING:
+        case CGI_OUTPUT_READING:
+        case CGI_LOCAL_REDIRECT_IO_PENDING:
+            handleRequest();
             if (_ioPendingState != NO_IO_PENDING)
                 break;
-        }
-    // fallthrough
-    case CGI_BODY_SENDING:
-    case CGI_OUTPUT_READING:
-    case CGI_LOCAL_REDIRECT_IO_PENDING:
-        handleRequest();
-        if (_ioPendingState != NO_IO_PENDING)
+        // fallthrough
+        case ERROR_LOCAL_REDIRECT_IO_PENDING:
+        case RESPONSE_SENDING:
+            Request::sendResponse();
             break;
-    // fallthrough
-    case ERROR_LOCAL_REDIRECT_IO_PENDING:
-    case RESPONSE_SENDING:
-        Request::sendResponse();
-        break;
-    default:
-        // never come here
-        break;
-  }
+        default:
+            // never come here
+            break;
+    }
 }
 
 }  // namespace http
