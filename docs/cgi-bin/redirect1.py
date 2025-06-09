4f17626a8a9efd5fbb90c1295d76680ad685d939
oonisidesu <eeeelllleeee24@gmail.com>
2025-06-09 22:47:29 +0900

    redirect1.py: Clean up output by removing unnecessary sys calls and success message

--- docs/cgi-bin/redirect1.py
+++ docs/cgi-bin/redirect1.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python3
-import sys
 
-print("Location: /redirect2.py", file=sys.stderr)
 print("Location: /redirect2.py")
 print("")
-print("Script redirect1.py executed successfully", file=sys.stderr)
-sys.stdout.flush()
-sys.stderr.flush()
-sys.exit(0)
